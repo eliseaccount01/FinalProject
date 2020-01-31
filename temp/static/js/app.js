256d74c9a59f0dd3470675dd2466e4815b840ae5
Christina Thiphavong <christinathip@gmail.com>
2020-01-30 19:28:01 -0600

    remove temp folder

--- temp/static/js/app.js
+++
@@ -1,9 +0,0 @@
-d3.json("/api/v1.0/statedatabase").then(data =>{
-  console.log("works");
-  console.log(data)
-});
-
-d3.json("/api/v1.0/drugdatabase").then(data =>{
-  console.log("works");
-  console.log(data)
-});
